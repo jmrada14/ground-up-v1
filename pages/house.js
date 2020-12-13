ed76b5210589444d610e0e1a010d27d1ae5ee6bf
juan <jmrada14@gmail.com>
2020-12-13 14:18:23 -0500

    test

--- pages/house.js
+++ pages/house.js
@@ -8,15 +8,13 @@ import MemberCard from "../components/MemberCard";
 export default function Home() {
   const [memberData, setMemberData] = React.useState([]);
   React.useEffect(() => {
-    fetchUrl("http://localhost:8000/api/members/chamber/house").then((res) =>
-      setMemberData(res)
-    );
+    fetchUrl("api/house").then((res) => setMemberData(res));
   }, []);
-  console.log(memberData);
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Ground up</title>
+        <title>House Of Representatives</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -30,31 +28,42 @@ export default function Home() {
 
         <h2>We can improve, so let's do it from the ground up!</h2>
         <div className={styles.grid}></div>
-        <label for="state-filter">Filter By State</label>
+        <label htmlFor="state-filter">Filter By State</label>
         <select name="filter" id="filterSelect">
           <option value="All">All</option>
-          {states.map((e) => (
-            <option value={e}>{e}</option>
+          {states.map((state, index) => (
+            <option key={`option-${index}`} value={state}>
+              {state}
+            </option>
           ))}
         </select>
-        <label for="state-filter">Filter By Party</label>
+        <label htmlFor="state-filter">Filter By Party</label>
         <select name="filter" id="filterSelect">
           <option value="All">All</option>
           <option value="D">Democrat</option>
           <option value="D">Republican</option>
         </select>
         <div className={styles.grid}>
-          {memberData?.map(({ name, party, role, state, twitter }) => {
+          {memberData?.map(({ name, party, role, state, twitter }, index) => {
             return (
               <>
                 <MemberCard
+                  key={`card-${index}`}
                   header={<h3>{name}</h3>}
                   description={
-                    <ul>
-                      <li>State: {state}</li>
-                      <li>Party: {party}</li>
-                      <li>Role: {role}</li>
-                      <li>Twitter: {twitter}</li>
+                    <ul key={`list-${index}`}>
+                      <li key={`listState-${index}`}>State: {state}</li>
+                      <li key={`listParty-${index}`}>Party: {party}</li>
+                      <li key={`listRole-${index}`}>Role: {role}</li>
+                      <li key={`listTwitter-${index}`}>
+                        Twitter: {twitter}{" "}
+                        <a
+                          className="mention-button"
+                          href="https://twitter.com/intent/tweet"
+                        >
+                          Tweet
+                        </a>
+                      </li>
                     </ul>
                   }
                 />
